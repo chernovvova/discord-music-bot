898e83bcb158a56b0538902df0c0885a91f75049
Vladimir Chernov <113360964+amomAA@users.noreply.github.com>
2024-03-31 21:15:19 +0300

    first commit

---
+++ src/main/java/com/musicbot/Config.java
@@ -0,0 +1,5 @@
+package com.musicbot;
+
+public class Config {
+    final static public String TOKEN = "MTIwMzMwNDY0NDcyNjc1MTMxMg.GBnN57.5gCH-j0GoWzYZWZwxPE76zDc1God2odR3Y-Aew";
+}
---
+++ src/main/java/com/musicbot/Main.java
@@ -0,0 +1,62 @@
+package com.musicbot;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
+
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.VoiceState;
+import discord4j.core.object.entity.Member;
+import discord4j.voice.AudioProvider;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class Main {
+    private static HashMap<String, Command> commands = new HashMap<>();
+
+    static {
+        commands.put("ping", event -> event.getMessage().getChannel().flatMap(channel -> channel.createMessage("pong")).then());
+    }
+
+    @SuppressWarnings("deprecation")
+    public static void main(String[] args) {
+        final AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
+        playerManager.getConfiguration()
+            .setFrameBufferFactory(NonAllocatingAudioFrameBuffer::new);
+        AudioSourceManagers.registerRemoteSources(playerManager);
+        final AudioPlayer player = playerManager.createPlayer();
+        AudioProvider provider = new LavaPlayerAudioProvider(player);
+
+        commands.put("join", event -> Mono.justOrEmpty(event.getMember())
+            .flatMap(Member::getVoiceState)
+            .flatMap(VoiceState::getChannel)
+            .flatMap(channel -> channel.join(spec -> spec.setProvider(provider)))
+            .then());
+        final TrackScheduler scheduler = new TrackScheduler(player);
+        commands.put("play", event -> Mono.justOrEmpty(event.getMessage().getContent())
+            .map(content -> Arrays.asList(content.split(" ")))
+            .doOnNext(command -> playerManager.loadItem(command.get(1), scheduler))
+            .then());
+
+        final GatewayDiscordClient client = DiscordClientBuilder.create(Config.TOKEN)
+        .build()
+        .login()
+        .block();
+
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+            .flatMap(event -> Mono.just(event.getMessage().getContent())
+                .flatMap(content -> Flux.fromIterable(commands.entrySet())
+                    .filter(entry -> content.startsWith('!' + entry.getKey()))
+                    .flatMap(entry -> entry.getValue().execute(event))
+                    .next())).subscribe();
+
+        client.onDisconnect().block();
+    }
+}
